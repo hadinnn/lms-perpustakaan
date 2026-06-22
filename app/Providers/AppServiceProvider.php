5e9d0f7cc0d269e67e768d1602ae3cae953a372b
hadinnn <mhafizhmuhyiddin_2428240127@mhs.mdp.ac.id>
2026-06-22 16:22:44 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+        \URL::forceScheme('https');
+        }   
         // Gunakan Tailwind untuk tampilan paginasi
         Paginator::useTailwind();
 
